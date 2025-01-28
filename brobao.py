dfe3d2d3012c00d8b91686890d8b550007c271dc
rayshih <rayshih@sysage.com.tw>
2025-01-29 01:07:48 +0800

    解析百葉箱
    解析百葉箱

--- brobao.py
+++ brobao.py
@@ -5,9 +5,9 @@ from Noise import Noise
 from Temperature import Temperature
 from Humidity import Humidity
 
-tty = '/dev/tty.usbmodem14401'
-# '/dev/ttyACM0'
-ser = serial.Serial(tty, 115200, timeout=0.5) # or make sure that both are running at the same baudrate
+#tty = '/dev/tty.usbmodem14401'
+tty = '/dev/ttyACM0'
+ser = serial.Serial(tty, 115200, timeout=0.2) # or make sure that both are running at the same baudrate
 ser.flushInput()
 
 modbus_signals = {
@@ -28,23 +28,34 @@ def handle_modbus_type(modbus_type, value):
 def main():
     # 假設有三種客戶類型
     count = 0
-
+    onoff = 0
     time.sleep(0.5) # include some sleep here also
+    init = "mesh init\n"
+    print(init)
+    ser.write(init.encode("iso8859-1"))
+    time.sleep(0.1)
+    init = "mesh target dst 0xc000\n"
+    print(init)
+    ser.write(init.encode("iso8859-1")) # group 位址
+    time.sleep(0.1)
+    line = ser.readline() # readline is not very nice, think about using read_all() or just read() instead.
+    print(line)
     while True:
         line = ser.readline() # readline is not very nice, think about using read_all() or just read() instead.
-        index = line.find(b">:")  # 找到 `>:` 的位置
+        index = line.find(b">: ")  # 找到 `>:` 的位置
         if index != -1:  # 確保 `>:` 存在
-            result = line[index + 2:].strip()  # 擷取 `>:` 後面的字串
-            print((line[index-7:index+2]).decode("iso8859"))
-            print(handle_modbus_type("temperature", result)) # Temperature signal processing. Value1: 120.0, Value2: 144.0
-        if(0 == count):
-            ser.write(b"mesh test net-send 82030110\n")
-            count = 1
-            print("Led On")
-        else:
-            ser.write(b"mesh test net-send 82030000\n")
-            count = 0
-            print("Led Off")
+            index0 = line.find(b"<0x")
+            print(line[index0:index+1])
+            result = line[index + 3:-4].strip()  # 擷取 `>:` 後面的字串
+            bytestr = result.decode("iso8859-1")
+            print(handle_modbus_type("temperature", bytes.fromhex(bytestr))) # Temperature signal processing. Value1: 120.0, Value2: 144.0
+            print(handle_modbus_type("humidity", bytes.fromhex(bytestr))) # Temperature signal processing. Value1: 120.0, Value2: 144.0
+            print(handle_modbus_type("noise", bytes.fromhex(bytestr))) # Temperature signal processing. Value1: 120.0, Value2: 144.0
+        cmd = f"mesh test net-send 82030{onoff:d}{count:02d}\n"
+        print(cmd)
+        ser.write(cmd.encode('iso8859-1'))
+        onoff = (onoff + 1) % 2
+        count = (count + 1) % 100
         time.sleep(1.5)
 
 if __name__ == "__main__":
